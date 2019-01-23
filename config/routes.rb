aa8c54d643b9655c218b5319f9b4da8eb89148a8
2357gi <2357.gi@gmail.com>
2019-01-23 20:50:57 +0900

    [fix] route.rb settings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   get 'show_pages/show'
   get 'show_pages/help'
   get 'show_pages/about'
-  # root 'application#hello'
+  # root 'application#hello
+  namespace :api do
+    namespace :v1 do
+      post 'existence', to: 'existence#post'
+    end
+  end
   root to: 'show_pages#show'
 end
