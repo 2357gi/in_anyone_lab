5d5e5103749411f52bd123b0085314e172fda075
2357gi <2357.gi@gmail.com>
2019-01-24 22:12:54 +0900

    [add] Browserに滞在中の人と総滞在時間を表示するように

--- app/controllers/api/v1/existence_controller.rb
+++ app/controllers/api/v1/existence_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::ExistenceController < ApplicationController
-
+  protect_from_forgery :except => [:post]
 
   def post
     # post されるパラメータは [{"name": "kento", "status": true}]
 
-    existence = params
+    existence = params[:existence]
+    baba = existence
+    babaname=existence[:name]
     data = Existence.find_by(name: existence[:name])
     data.status = existence[:status]
     time = Time.now
@@ -12,9 +14,10 @@ class Api::V1::ExistenceController < ApplicationController
     if (data.changed?)
       if (data.status == true)
         data.update(status: true, enter_time: time)
+        logger.debug data.errors.inspect
       else
         diff = time - Time.parse(data.enter_time.to_s)
-        total += diff / 60
+        total = diff / 60 + data.total
         data.update(status: false, exit_time: time,
                     total: total)
       end
