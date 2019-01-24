db3d481de7cbf3b51cb3ae321c4d930c8a2332b1
2357gi <2357.gi@gmail.com>
2019-01-24 21:31:41 +0900

    [add] postされた出席情報をwebで表示するように

--- app/controllers/api/v1/existence_controller.rb
+++ app/controllers/api/v1/existence_controller.rb
@@ -1,25 +1,22 @@
-require 'date'
-class Api::V1::ExistanceController < ApplicationController
+class Api::V1::ExistenceController < ApplicationController
+
+
   def post
-    time = DataTime.now
-    existences = params
+    # post されるパラメータは [{"name": "kento", "status": true}]
 
+    existence = params
+    data = Existence.find_by(name: existence[:name])
+    data.status = existence[:status]
+    time = Time.now
 
-    # existenceが[{name:'hoge', status:1},{name:'huga', status:0} ]
-    existences.each do |f|
-      data = Existence.find_by(name: f.name)
-
-      if (f.status.changed?)
-        if (f.status == true)
-          data.update(status: true, enter_time: time)
-        else
-          diff = time - data.enter_time
-          hour = diff.strftime("%H").to_i
-          min = diff.strftime("%M").to_i
-          total = hour * 60 + min
-          data.update(status: false, exit_time: time,
-                      total: total)
-        end
+    if (data.changed?)
+      if (data.status == true)
+        data.update(status: true, enter_time: time)
+      else
+        diff = time - Time.parse(data.enter_time.to_s)
+        total = diff / 60
+        data.update(status: false, exit_time: time,
+                    total: total)
       end
     end
   end
